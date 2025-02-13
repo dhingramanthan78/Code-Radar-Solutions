b4798fbceb5a0176de35f225d35f5c732eab9b87
Manthan Dhingra <196765005+dhingramanthan78@users.noreply.github.com>
2025-02-14 01:14:21 +0530

    Solution to Simple Calculator

---
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+int main(){
+    int a,b,result;
+    char operation;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    scanf("%s", &operation);
+    switch(operation){
+        case '+':
+         result=a+b;
+         printf(result);
+         break;
+        case '-':
+         result=a-b;
+         printf(result);
+         break;
+        case '*':
+         result=a*b;
+         printf(result);
+         break;
+        case '/':
+           if (b!=0){
+            result=a/b;
+            printf(result);
+            break;
+           }
+           else{
+            printf("The Division of the number is not possible\n");
+            break;
+           }
+         default:
+          printf("error");
+    }
+}
+    
