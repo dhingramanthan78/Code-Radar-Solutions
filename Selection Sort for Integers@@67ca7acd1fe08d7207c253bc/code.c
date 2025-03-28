0f025c9247e7293711670ae18c37fe86651e5617
Manthan Dhingra <196765005+dhingramanthan78@users.noreply.github.com>
2025-03-28 19:15:13 +0530

    Solution to Selection Sort for Integers

--- Selection Sort for Integers@@67ca7acd1fe08d7207c253bc/code.c
+++ Selection Sort for Integers@@67ca7acd1fe08d7207c253bc/code.c
@@ -1,35 +1,45 @@
 #include <stdio.h>
-int selectionSort(int arr[],int n){
-    int i,j,minindex;
-    for (int i=1;i<=n;i++){
-        for (int j=i+1;j<=n;j++){
-        if(arr[j]<arr[minindex]){
-            minindex=j;
+
+void selectionSort(int arr[], int n) {
+    int i, j, minindex;
+    
+    for (i = 0; i < n - 1; i++) {  // Loop from 0 to n-1
+        minindex = i;
+        
+        for (j = i + 1; j < n; j++) {  // Compare with remaining elements
+            if (arr[j] < arr[minindex]) {
+                minindex = j;
+            }
         }
-        minindex=i;
-    }
-    }
-    if(minindex!=i){
-        int temp=arr[i];
-        arr[i]=arr[minindex];
-        arr[minindex]=temp;
+        
+        // Swap if minindex is different from i
+        if (minindex != i) {
+            int temp = arr[i];
+            arr[i] = arr[minindex];
+            arr[minindex] = temp;
+        }
     }
 }
-void printarray(int arr,int size){
-    for (int i=1;i<=size;i++){
-        printf("%d", arr[i]);
+
+void printarray(int arr[], int size) {  // Correct parameter type
+    for (int i = 0; i < size; i++) {    // Correct loop range
+        printf("%d ", arr[i]);  // Remove '&' to print values
     }
     printf("\n");
 }
-    
-    int main(){
+
+int main() {
     int n;
     scanf("%d", &n);
-    int arr[n];
-    for (int i=0;i<n;i++){
+    
+    int arr[n];  // Declare array
+    
+    for (int i = 0; i < n; i++) {  // Use correct 0-based index
         scanf("%d", &arr[i]);
     }
-    selectionSort(arr ,n);
-    printarray(arr ,n);
+    
+    selectionSort(arr, n);
+    printarray(arr, n);
+    
     return 0;
 }
